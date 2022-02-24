e7a84565c74873782ebd4feda57405212b8fedcb
karthikbenki <karthikmc007@gmail.com>
2022-02-24 17:45:31 +0530

    Added:Linked List Class

--- src/com/blz/ds/LinkedList.java
+++ src/com/blz/ds/LinkedList.java
@@ -1,7 +1,18 @@
 package com.blz.ds;
 
-public class LinkedList {
+public class LinkedList  {
+    Node head =null;
+    class Node{
+        Object data;
+        Node Next=null;//next is a reference
+
+        public Node(Object data) {
+            this.data = data;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome To Linked list !!!!");
+
     }
 }
